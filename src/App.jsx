62f025ae847d67825efa7a9746a0ea64891914b7
Mayank Madhav <mayank.madhav@hotmail.com>
2025-05-08 20:06:01 +0530

    Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [myFavoriteThings,setMyFavoriteThings] = React.useState([]);
+/**
+     * Challenge: Convert the code below to use an array
+     * held in state instead of a local variable. Initialize 
+     * the state array as an empty array
+     * 
+     * Don't worry about fixing `addFavoriteThing` quite yet.
+     */
+
+const allFavoriteThings = ["💦🌹", "😺", "💡🫖", "🔥🧤", "🟤🎁", 
+"🐴", "🍎🥧", "🚪🔔", "🛷🔔", "🥩🍝"]
+const thingsElements = myFavoriteThings.map(thing => <p key={thing}>{thing}</p>)
+
+function addFavoriteThing() {
+  // We'll work on this next, nothing to do here yet.
+  setMyFavoriteThings(prevState => [...prevState,allFavoriteThings[(prevState.length)%allFavoriteThings.length]])
+}
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+    <button onClick={addFavoriteThing}>Add item</button>
+    <section aria-live="polite">
+      {thingsElements}
+    </section>
+  </main>
   )
 }
 
